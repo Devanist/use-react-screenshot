574141fe2b77b82f88d646109d276e352d5455ac
Przemysław Lewandowski <devanist@outlook.com>
2022-02-22 13:06:51 +0100

    v2

--- index.d.ts
+++ index.d.ts
@@ -7,11 +7,5 @@ export type Options = import('html2canvas').Options;
 
 type TakeScreenShot = (node: HTMLElement, options?: Partial<Options>) => string;
 
-type UseScreenshot = ({ type, quality }: Partial<UseScreenshotOptions> | undefined) => [string, TakeScreenShot, { error: Error }]
-
-type CreateFileName = (extension: string, parts: string[]) => string;
-
-declare const useScreenshot: UseScreenshot;
-declare const createFileName: CreateFileName;
-
-export default { createFileName, useScreenshot };
+export function useScreenshot ({ type, quality }: Partial<UseScreenshotOptions> | undefined): [string, TakeScreenShot, { error: Error }]
+export function createFileName (extension: string, parts: string[]): string;
